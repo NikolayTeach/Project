b9dae24293a580b2a5966b0a7d3e910d41b1a9c0
Nikolos123 <Isponec_2>
2023-01-24 21:37:48 +0300

    добавления параметра другой вариант

---
+++ lesson_6/decs/deco_params_func_6.py
@@ -0,0 +1,38 @@
+"""Простейший декоратор-функция с параметром"""
+#Расчет времени 10 запусков
+import time
+import requests
+
+
+def decorator(iters):
+    """Внешняя функция (формально - декоратор)"""
+    def real_decorator(func):
+        """Сам декоратор"""
+        def wrapper(*args, **kwargs):
+            """Обертка"""
+            total = 0
+            for _ in range(iters):
+                start = time.time()
+                return_value = func(*args, **kwargs)
+                end = time.time()
+                total = total + (end-start)
+            print(f'Среднее время выполнения: {round(total/iters, 2)} секунд')
+            return return_value
+
+        return wrapper
+    return real_decorator
+
+
+
+@decorator(iters=10)
+def get_wp(url):
+    """Запрос"""
+    res = requests.get(url)
+    return res
+
+
+get_wp('https://google.com')
+get_wp('https://ya.ru')
+
+#наличии 2х декораторов
+#Какой из декораторов отработает первым?
